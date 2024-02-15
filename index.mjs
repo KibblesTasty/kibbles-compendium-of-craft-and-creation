e6d6f7177bee59203e9d7d347c1462320fba64fc
Joseph <JPMeehan5@Gmail.com>
2024-02-15 19:41:09 +0000

    Spell school update & feature types

--- index.mjs
+++ index.mjs
@@ -1,4 +1,16 @@
 Hooks.once("init", () => {
   // Make Psionics a valid spellcasting school
-  CONFIG.DND5E.spellSchools.psi = "KCCC.SchoolPsi";
+  CONFIG.DND5E.spellSchools.psi = {
+    label: "KCCC.SchoolPsi",
+    icon: "modules/kibbles-compendium-of-craft-and-creation/assets/psionics.svg",
+    fullKey: "psionic",
+    reference: ""
+  };
+
+  // Add new class feature types for validation
+  foundry.utils.mergeObject(CONFIG.DND5E.featureTypes.class.subtypes, {
+    psionicTalent: "KCCC.PsionicTalent",
+    psionicDiscipline: "KCCC.PsionicDiscipline",
+    upgrade: "KCCC.Upgrade"
+  })
 });
