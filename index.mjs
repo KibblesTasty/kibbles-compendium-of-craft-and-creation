0d271ac63b6826dd8fb80d18be64342c91b5046a
Joseph <JPMeehan5@Gmail.com>
2024-07-23 16:52:21 -0700

    Subclass updates

--- index.mjs
+++ index.mjs
@@ -1,19 +1,26 @@
-Hooks.once("init", () => {
+Hooks.once('init', () => {
   // Make Psionics a valid spellcasting school
   CONFIG.DND5E.spellSchools.psi = {
-    label: "KCCC.SchoolPsi",
-    icon: "modules/kibbles-compendium-of-craft-and-creation/assets/psionics.svg",
-    fullKey: "psionic",
-    reference: ""
+    label: 'KCCC.SchoolPsi',
+    icon: 'modules/kibbles-compendium-of-craft-and-creation/assets/psionics.svg',
+    fullKey: 'psionic',
+    reference: '',
   };
 
   // Adds KCCC as a suggested book when you define the source of an item
-  CONFIG.DND5E.sourceBooks.KCCC = "Kibbles' Compendium of Craft and Creation"
+  CONFIG.DND5E.sourceBooks.KCCC = "Kibbles' Compendium of Craft and Creation";
 
   // Add new class feature types for validation
   foundry.utils.mergeObject(CONFIG.DND5E.featureTypes.class.subtypes, {
-    psionicTalent: "KCCC.PsionicTalent",
-    psionicDiscipline: "KCCC.PsionicDiscipline",
-    upgrade: "KCCC.Upgrade"
-  })
+    psionicTalent: 'KCCC.PsionicTalent',
+    psionicDiscipline: 'KCCC.PsionicDiscipline',
+    upgrade: 'KCCC.Upgrade',
+  });
+
+  CONFIG.DND5E.skills.psi = {
+    label: 'Psionics',
+    ability: 'int',
+    fullKey: 'psionics',
+    icon: 'modules/kibbles-compendium-of-craft-and-creation/assets/psionics.svg',
+  };
 });
